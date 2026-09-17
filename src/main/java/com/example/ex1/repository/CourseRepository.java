ffeeabdb0d05801f77092999174da1feb89cea6f
= <=>
2026-09-17 21:53:43 +0530

    Added course management CRUD

--- src/main/java/com/example/ex1/repository/CourseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.ex1.repository;
-
-import com.example.ex1.entity.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseRepository extends JpaRepository<Course, Long> {
-}
